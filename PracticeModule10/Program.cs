638a1be3dfb762eec77c1e0d4bc031ddbd10596b
Islam Shopalov <116016315+1Sl4M@users.noreply.github.com>
2023-11-12 22:16:18 +0600

    Add Practice Module 10

---
+++ PracticeModule10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeModule10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+
+            IWorker[] team = { new Worker() };
+            
+            Team constructionTeam = new Team(team);
+            
+            constructionTeam.BuildHouse(house);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticeModule10/TeamLeader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeModule10
+{
+    public class TeamLeader : IWorker
+    {
+        public void Build(IPart part, House house)
+        {
+            if (part.IsBuilt)
+            {
+                Console.WriteLine($"TeamLeader: {part.Name} is already built.");
+            }
+            else
+            {
+                Console.WriteLine($"TeamLeader: {part.Name} is not built.");
+            }
+        }
+    }
+}
