5c581743ca625ae385f272e203a5b079c5409b53
Islam Shopalov <116016315+1Sl4M@users.noreply.github.com>
2023-11-26 18:19:38 +0600

    Add files via upload

---
+++ HWModule13/BankQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWModule13
+{
+    public class BankQueue
+    {
+        private Queue<Client> clientsQueue = new Queue<Client>();
+
+        public int Count => clientsQueue.Count;
+
+        public void Enqueue(Client client)
+        {
+            clientsQueue.Enqueue(client);
+        }
+
+        public Client Dequeue()
+        {
+            return clientsQueue.Dequeue();
+        }
+    }
+}
---
+++ HWModule13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWModule13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankQueue bankQueue = new BankQueue();
+
+            while (true)
+            {
+                Console.WriteLine("1. Встать в очередь");
+                Console.WriteLine("2. Обслужить следующего клиента");
+                Console.WriteLine("3. Выйти из программы");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите ИИН клиента:");
+                        string iin = Console.ReadLine();
+                        Console.WriteLine("Выберите услугу (1. Кредитование, 2. Открытие вклада, 3. Консультация):");
+                        int serviceType = int.Parse(Console.ReadLine());
+
+                        ServiceType selectedService = (ServiceType)serviceType;
+                        Client newClient = new Client(iin, selectedService);
+
+                        bankQueue.Enqueue(newClient);
+                        Console.WriteLine($"Клиент {iin} добавлен в очередь для {selectedService}");
+                        Console.WriteLine($"Текущая длина очереди: {bankQueue.Count}");
+                        break;
+
+                    case 2:
+                        if (bankQueue.Count > 0)
+                        {
+                            Client servicedClient = bankQueue.Dequeue();
+                            Console.WriteLine($"Обслужен клиент {servicedClient.IIN} для {servicedClient.Service}");
+                            Console.WriteLine($"Текущая длина очереди: {bankQueue.Count}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Очередь пуста.");
+                        }
+                        break;
+
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
